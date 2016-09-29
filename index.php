758deda77cf283d286d73a49b59a4114477c759b
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-09-29 13:34:59 -0400

    added some code for class lesson 1

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
-	echo "hello world";
-
+// THIS IS HOW YOU PRINT TEXT IN PHP
+	echo '"hello world';
+	// THIS IS HOW YOU STORE A VALUE IN A VARIABLE
+	$myvar = 'Some Text I want to store in a variable';
+	echo '<br>';
+	// THIS IS THE DIFFERENCE BETWEEN SINGLE QUOTES AND DOUBLE QUOTES
+	echo '$myvar';
+	echo '<br>';
+	// DOUBLE QUOTES PRINT THE VALUE WHILE SINGLE QUOTES PRINT WHAT IS BETWEEN THE QUOTES
+	echo"$myvar";
 ?>
